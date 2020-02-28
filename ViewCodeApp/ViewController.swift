ec18f7e873faca955ce0215c426417bd5902fb9e
Bruno Cortez <bruno.mcortez@gmail.com>
2020-02-28 19:29:07 -0300

    Initial Commit

---
+++ ViewCodeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewCodeApp
+//
+//  Created by Bruno Cortez on 2/28/20.
+//  Copyright © 2020 Bruno Cortez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
